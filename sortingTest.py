881613db8cf7738b42b31b3485b52e1ecd8f8020
KITTICHAI-WANNAPRUEK <kittichai.wa@kkumail.com>
2025-12-11 01:23:23 +0700

    Implement sorting algorithms selection in sortingTest.py

---
+++ sortingTest.py
@@ -0,0 +1,17 @@
+from buble import buble_sort
+from quicksort import quick_sort
+
+n = int(input("Enter the number of array elements:"))
+arr = []
+for i in range(n):
+  number = int(input("Enter the number:"))
+  arr.append(number)
+#print(f'your array is {arr}')
+print("\n 1.Bubble \n 2.Quick")
+algor = int(input("Choose algorithm: "))
+ans = []
+if (algor==1):
+  ans = bubble_sort(arr)
+elif (algor==2):
+  ans = quick_sort(arr)
+print(f'result {ans}')
