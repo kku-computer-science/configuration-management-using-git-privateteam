f935f39fdbbd9a7f2bb48629e08edec013df06a6
Kittichai Wannapruek <kittichai.wa@kkumail.com>
2025-12-11 10:17:57 +0700

    Improve input prompts and algorithm name

--- Project/sortingTest.py
+++ Project/sortingTest.py
@@ -4,14 +4,19 @@ from quick_sort import quick_sort
 n = int(input("Enter the number of array elements:"))
 arr = []
 for i in range(n):
-  number = int(input("Enter the number:"))
+  number = int(input("Enter a number:"))
   arr.append(number)
-#print(f'your array is {arr}')
-print("\n 1.Bubble \n 2.Quick")
+print("\n 1.Bubble_Sort \n 2.Quick_Sort")
 algor = int(input("Choose algorithm: "))
+chooseAlgor = ""
 ans = []
 if (algor==1):
   ans = bubble_sort(arr)
+  chooseAlgor = "Bubble_Sort"
 elif (algor==2):
   ans = quick_sort(arr)
+  chooseAlgor = "Quick_Sort"
+else:
+  print("Invalid input")
+print(f'Algorithm: {chooseAlgor}')
 print(f'result {ans}')
